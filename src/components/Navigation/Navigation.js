40025d5325171b3328e857dc91aa6a63d429182e
Charith Lakshitha <charithlakshitha@outlook.com>
2021-09-14 00:44:57 +0530

    added basic components

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navigation = () => {
+  return (
+    <nav style={{ display: "flex", justifyContent: "flex-end" }}>
+      <p className="f4 link dim white pa3 pointer ph3 pv2 dib white bg-mid-gray br2 mr3">
+        Sign Out
+      </p>
+    </nav>
+  );
+};
+
+export default Navigation;
